cec5b871827d9c0d93236e9da8acb22949eea620
Anay Infowind <anay.infowind@gmail.com>
2021-10-30 14:59:51 +0530

    Initial commit

---
+++ settings.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of The Course Module Navigation Block
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Settings for course module navigation.
+ *
+ * @package    block_tb_leeloolxpvimeo_nav
+ * @copyright  2020 Leeloo LXP (https://leeloolxp.com)
+ * @author     Leeloo LXP <info@leeloolxp.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($ADMIN->fulltree) {
+    require_once($CFG->dirroot . '/blocks/tb_leeloolxpvimeo_nav/lib.php');
+
+    $setting = new admin_setting_configtext(
+        'block_tb_leeloolxpvimeo_nav/license',
+        get_string('license', 'block_tb_leeloolxpvimeo_nav'),
+        get_string('license', 'block_tb_leeloolxpvimeo_nav'),
+        0
+    );
+    //$setting->set_updatedcallback(block_tb_leeloolxpvimeo_nav_updateconf());
+    $settings->add($setting);
+
+    $setting = new admin_setting_configleeloolxpvimeo_nav('block_tb_leeloolxpvimeo_nav/settingsjson', '', '', '', PARAM_RAW);
+    $settings->add($setting);
+}
---
+++ version.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of The Course Module Navigation Block
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Course module navigation version.
+ *
+ * @package    block_tb_leeloolxpvimeo_nav
+ * @copyright  2020 Leeloo LXP (https://leeloolxp.com)
+ * @copyright  based on work by 2019 Pimenko <contact@pimenko.com> <pimenko.com>
+ * @author     Leeloo LXP <info@leeloolxp.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'block_tb_leeloolxpvimeo_nav';
+$plugin->release = '1.0.2';
+$plugin->version = 2021101101;
+$plugin->requires = 2018051700; // Moodle 3.5 and above.
+$plugin->maturity = MATURITY_STABLE;
+$plugin->dependencies = array(
+    'local_leeloolxpcontentapi' => 2021031907,
+    'mod_leeloolxpvimeo' => 2015051101
+);
